b4851e0f88a12a800f37dbf3533ff36f8230f94e
Krishna Choudhary <104185486+kc-conquers-code11@users.noreply.github.com>
2025-07-26 23:59:19 +0530

    Create PrimeOpt.java
    Checks if no is prime or not.

---
+++ PrimeOpt.java
@@ -0,0 +1,27 @@
+// Jva Program  to check whether the number is prime or not
+import java.util.*;
+
+public class PrimeOpt {
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false; 
+        }
+        if (n == 2) {
+            return true; 
+        }
+        if (n % 2 == 0) {
+            return false; 
+        }
+        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Is the given number prime? ");
+        System.out.println(isPrime(5)); // true
+    }
+}
